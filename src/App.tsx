c6a7061457a8d708dfef35797654950ce845fac0
driss292 <drisskaci@outlook.fr>
2025-02-17 17:56:45 +0100

    Board is OK Deck is coming

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,54 @@
 import { RiResetLeftFill } from "react-icons/ri";
 import style from "./App.module.css";
-import { useState } from "react";
-import { dataSeat as initialDataSeat } from "./utils/data";
+import { useMemo, useState } from "react";
+import { dataSeat, suits } from "./utils/data";
+import { generateCardBySuit, getCardColor } from "./utils/functions";
+
+// Interface pour les cartes
+interface ICard {
+  value: string; // La valeur de la carte, par exemple 'A', '10', etc.
+  suit: string; // La couleur de la carte, par exemple 'hearts', 'diamonds'
+  src: string; // Le chemin de l'image de la carte, par exemple '/images/ace_of_hearts.png'
+}
+
 function App() {
-  const [dataSeat, setDataSeat] = useState(initialDataSeat);
+  // Initialisation de l'état des joueurs
+  const initialPlayerState = dataSeat.reduce((acc, seat) => {
+    acc[`player${seat.id}`] = {
+      cards: [], // Pas de cartes pour le moment
+      posX: seat.posX,
+      posY: seat.posY,
+      score: 0, // Score à 0 par défaut
+    };
+    return acc;
+  }, {} as { [key: string]: { cards: ICard[]; posX: number; posY: number; score: number } });
+
+  const initialFlopState = {
+    "flop-1": null,
+    "flop-2": null,
+    "flop-3": null,
+  } as { [key: string]: ICard | null };
+
+  // Utilisation de l'état pour les informations des joueurs
+  const [dataPlayerSeat, setDataPlayerSeat] = useState(initialPlayerState);
+  const [flopCards, setFlopCards] = useState(initialFlopState);
+  const [turnCard, setTurnCard] = useState(null); // La carte du turn
+  const [riverCard, setRiverCard] = useState(null); // La carte de la river
+
+  // const cardColor = getCardColor(suit);
+  // if (!cardColor) {
+  //   console.error(`Invalid suit provided: ${suit}`);
+  //   return null;
+  // }
+
+  // const { src, color } = cardColor;
+
+  // Utiliser useMemo pour éviter la régénération des cartes à chaque re-render
+  // const cards = useMemo(() => generateCardBySuit(), []); // Appel sans argument
+
+  //     // Récupérer les données de la couleur et du symbole pour chaque suit
+  // const suitData = getCardColor(suit);
+
   return (
     <main className="App">
       <div className={style.table}>
@@ -34,9 +79,72 @@ function App() {
           </span>
           <RiResetLeftFill style={{ fontSize: "40px" }} />
         </div>
-        <div className={style.boardContainer}>{/* <Board /> */}</div>
+        {/* BOARD */}
+        <div className={style.boardContainer}>
+          {/* Flop */}
+          <div
+            className={style.cardZone}
+            style={{ display: "flex", marginRight: "10px" }}
+          >
+            <span
+              className={style.titleZone}
+              style={{
+                top: "55px",
+                left: "47px",
+              }}
+            >
+              Flop
+            </span>
+            {Object.entries(flopCards).map(([key, card], index) => (
+              <div
+                key={key}
+                style={{ marginLeft: index > 0 ? "5px" : "0px" }}
+                className={
+                  card ? style.cardPlaceholderOfCard : style.cardPlaceholder
+                }
+              ></div>
+            ))}
+          </div>
+
+          {/* Turn */}
+          <div className={style.cardZone} style={{ marginRight: "10px" }}>
+            <span
+              className={style.titleZone}
+              style={{
+                top: "55px",
+                left: "131px",
+              }}
+            >
+              Turn
+            </span>
+            {turnCard ? (
+              <div className={style.cardPlaceholderOfCard}></div>
+            ) : (
+              <div className={style.cardPlaceholder}></div>
+            )}
+          </div>
+
+          {/* River */}
+          <div className={style.cardZone}>
+            <span
+              className={style.titleZone}
+              style={{
+                top: "55px",
+                left: "175px",
+              }}
+            >
+              River
+            </span>
+            {riverCard ? (
+              <div className={style.cardPlaceholderOfCard}></div>
+            ) : (
+              <div className={style.cardPlaceholder}></div>
+            )}
+          </div>
+        </div>
+
+        {/* SEAT */}
         {dataSeat.map((data) => (
-          // <Seat key={data.id} id={data.id} posX={data.posX} posY={data.posY} />
           <div
             key={data.id}
             id={`player-${data.id}`}
@@ -48,14 +156,56 @@ function App() {
           >
             <div className={style.id}>player {data.id}</div>
             <div className={style.cardContainer}>
-              <div className={style.card}></div>
-              <div className={style.card}></div>
-              {/* <Placeholder id={`player-${data.id}-card-1`} />
-              <Placeholder id={`player-${data.id}-card-2`} /> */}
+              {/* Placeholders pour les cartes */}
+              <div className={style.cardPlaceholder}></div>
+              <div className={style.cardPlaceholder}></div>
             </div>
-            <div className={style.score}>00.00%</div>
+            <div className={style.score}>
+              {dataPlayerSeat[`player${data.id}`].score === 0
+                ? "00.00"
+                : `${dataPlayerSeat[`player${data.id}`].score}`}
+              %
+            </div>
           </div>
         ))}
+      </div>
+
+      {/* DECK */}
+      <div className={style.deck}>
+        {suits.map((suit) => {
+          const suitData = getCardColor(suit.content);
+          return (
+            <div key={suit.id} className={style.suits}>
+              <div className={`${style.block} ${suitData?.color ?? ""}`}>
+                {suitData && (
+                  <img
+                    src={suitData.src}
+                    alt={suit.content}
+                    className={style.symbol}
+                  />
+                )}
+              </div>
+              {/* {cards.map((card) => {
+                const cardColor = getCardColor(card.suit);
+                return (
+                  <div
+                    key={`${card.suit}-${card.value}`}
+                    className={style.card}
+                  >
+                    <p className={`${style.value} ${cardColor?.color ?? ""}`}>
+                      {card.value}
+                    </p>
+                    <img
+                      className={style.suit}
+                      src={cardColor?.src ?? ""}
+                      alt={card.suit}
+                    />
+                  </div>
+                );
+              })} */}
+            </div>
+          );
+        })}
       </div>
     </main>
   );
--- src/utils/data.ts
+++ src/utils/data.ts
@@ -3,54 +3,96 @@ export const dataSeat = [
     id: 1,
     posX: 290,
     posY: 300,
+    type: "player", // Siège de joueur
   },
   {
     id: 2,
     posX: 145,
     posY: 280,
+    type: "player",
   },
   {
     id: 3,
     posX: 75,
     posY: 155,
+    type: "player",
   },
   {
     id: 4,
     posX: 145,
     posY: 40,
+    type: "player",
   },
   {
     id: 5,
     posX: 290,
     posY: 5,
+    type: "player",
   },
   {
     id: 6,
     posX: 430,
     posY: 5,
+    type: "player",
   },
   {
     id: 7,
     posX: 575,
     posY: 40,
+    type: "player",
   },
   {
     id: 8,
     posX: 640,
     posY: 155,
+    type: "player",
   },
   {
     id: 9,
     posX: 575,
     posY: 280,
+    type: "player",
   },
   {
     id: 10,
     posX: 430,
     posY: 300,
+    type: "player",
   },
 ];
 
+export const cardZones = [
+  {
+    id: "flop-1",
+    posX: 250,
+    posY: 175,
+    type: "cardZone", // Zone pour la première carte du flop
+  },
+  {
+    id: "flop-2",
+    posX: 300,
+    posY: 175,
+    type: "cardZone", // Zone pour la deuxième carte du flop
+  },
+  {
+    id: "flop-3",
+    posX: 350,
+    posY: 175,
+    type: "cardZone", // Zone pour la troisième carte du flop
+  },
+  {
+    id: "turn",
+    posX: 420,
+    posY: 175,
+    type: "cardZone", // Zone pour la carte du turn
+  },
+  {
+    id: "river",
+    posX: 465,
+    posY: 175,
+    type: "cardZone", // Zone pour la carte de la river
+  },
+];
 export const ranks = [
   "2",
   "3",
